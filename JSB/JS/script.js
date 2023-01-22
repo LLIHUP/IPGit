b016937ca8b9f8b226e4ec0929dd06a5dd4e06b7
Sasha <LLIHUP@gmail.com>
2023-01-22 14:55:02 +0200

    Практика3

--- JSB/JS/script.js
+++ JSB/JS/script.js
@@ -177,58 +177,122 @@
 // }
 
 
-let num = 50;
-// while (num <= 55) {
-//     console.log(num);
-//     num++;
-// }
+// let num = 50;
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
 
-// do {
-//     console.log(num);
-//     num++;
-// }
+// // do {
+// //     console.log(num);
+// //     num++;
+// // }
 
-// while (num < 55);
+// // while (num < 55);
 
-// for (let i = 1; i < 10; i++) {
-//     if (i === 6) {
-//         // break;
-//         continue;
-//     }
+// // for (let i = 1; i < 10; i++) {
+// //     if (i === 6) {
+// //         // break;
+// //         continue;
+// //     }
 
+// //     console.log(i);
+
+// // }
+
+
+// for (let i = 0; i < 3; i++) {
 //     console.log(i);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(j);
+//     }
+// };
 
+// let result = '';
+// const lengh = 7;
+
+// for (let i = 1; i < lengh; i++) {
+//     for (let j = 0; j < i; j++) {
+
+//         result += '*';
+
+//     }
+//     result += '\n';
 // }
 
+// console.log(result);
 
-for (let i = 0; i < 3; i++) {
+// first: for (let i = 0; i < 3; i++) {
+//     console.log(`first level: ${i}`);
+//     for (let j = 0; j < 3; j++) {
+//         console.log(`second level: ${j}`);
+//         for (let k = 0; k < 3; k++) {
+//             if (k === 2) continue first;
+//             console.log(`third level: ${k}`);
+//         }
+//     }
+// };
+
+
+for (i = 5; i <= 10; i++) {
     console.log(i);
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
-    }
 };
 
-let result = '';
-const lengh = 7;
 
-for (let i = 1; i < lengh; i++) {
-    for (let j = 0; j < i; j++) {
+for (let i = 20; i > 9; i--) {
+    if (i == 12) {
+        break;
+    };
+    console.log(i);
+};
 
-        result += '*';
-
+for (let i = 2; i <= 10; i++) {
+    if (i % 2 == 0) {
+        console.log(i);
     }
-    result += '\n';
+
 }
 
-console.log(result);
 
-first: for (let i = 0; i < 3; i++) {
-    console.log(`first level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) continue first;
-            console.log(`third level: ${k}`);
-        }
+for (let i = 2; i <= 16; i++) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
     }
-};
+}
+let num = 50;
+// // while (num <= 55) {
+// //     console.log(num);
+// //     num++;
+// // }
+
+let i = 2;
+while (i <= 16) {
+    if (i % 2 === 0) {
+        continue;
+    } else {
+        console.log(i);
+        i++;
+    }
+}
+let result = [];
+for (let i = 5; i < 11; i++) {
+    result.push(i++);
+    console.log(i);
+}
+
+const arrayOfNumbers = [];
+
+for (let i = 5; i < 11; i++) {
+    arrayOfNumbers[i - 5] = i;
+}
+
+console.log(arrayOfNumbers);
+return arrayOfNumbers;
+
+for (let i = 20; i > 9; i--) {
+    if (i === 13) break;
+
+    console.log(i);
+}
