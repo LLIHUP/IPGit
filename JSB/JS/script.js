8462d2ddd7fe9041144e1b5850c8870a6ae94631
Sasha <LLIHUP@gmail.com>
2023-01-29 16:59:23 +0200

    переход в новый файл

--- JSB/JS/script.js
+++ JSB/JS/script.js
@@ -296,3 +296,25 @@ for (let i = 20; i > 9; i--) {
 
     console.log(i);
 }
+
+const arr = [3, 5, 8, 16, 20, 23, 50];
+const result = [];
+
+for (let i = 0; i < arr.length; i++) {
+    result[i] = arr[i];
+}
+
+console.log(result);
+return result;
+
+const data = [5, 10, 'Shopping', 20, 'Homework'];
+
+for (let i = 0; i < data.length; i++) {
+    if (typeof(data[i]) === 'number') {
+        data[i] = data[i] * 2;
+    } else if (typeof(data[i]) === 'string') {
+        data[i] = `${data[i]} - done`;
+    }
+}
+console.log(data);
+return data;
