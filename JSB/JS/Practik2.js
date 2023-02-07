ae7a22af5acaeb3e2c72444c6f89f3e9c7faa8f0
Sasha <LLIHUP@gmail.com>
2023-02-07 22:44:29 +0200

    функции

---
+++ JSB/JS/Practik2.js
@@ -0,0 +1,189 @@
+'use strict';
+
+
+
+
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+//     b = prompt('На сколько оцените его?', ''),
+//     c = prompt('Один из последних просмотренных фильмов?', ''),
+//     d = prompt('На сколько оцените его?', '');
+
+
+// persona.movies[a] = [b];
+// persona.movies[c] = [d];
+// console.log(persona);
+// const Tom = prompt('какой актур?', '');
+// const nameActors = 'Имя';
+// persona.actors[nameActors] = Tom;
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+// const persona = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         persona.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+
+// }
+
+// let num = 0;
+// while (num < 2) {
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//         b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         persona.movies[a] = b;
+//         console.log('done');
+//         num++;
+//     } else {
+//         console.log('error');
+//         num--;
+
+//     }
+// }
+
+
+
+
+//     if (persona.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// } else if (persona.count >= 10 && persona.count < 30) {
+//     console.log('Вы классический зритель');
+// } else if (persona.count >= 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console.log('Произошла ошибка');
+// }
+
+// console.log(persona);
+// let num = 20;
+
+// function showFirstMessage(text) {
+//     console.log(text);
+//     let num = 10;
+//     console.log(num);
+// }
+
+// showFirstMessage('hello world');
+// console.log(num);
+
+// function calc(a, b) {
+//     return (a + b);
+// }
+
+// console.log(calc(4, 3));
+// console.log(calc(5, 6));
+
+// function ret() {
+//     let num = 50;
+//     return num;
+// }
+
+// const anotherNum = ret();
+// console.log(anotherNum);
+
+
+// const logger = function() {
+//     console.log('hello');
+// };
+// logger();
+
+// const calc = (a, b) => {
+//     console.log('text');
+//     return a + b;
+// };
+// console.log(calc(1, 2));
+
+
+
+// // const usdCurr = 50;
+// const eurCurr = 32;
+
+// function convert(amount, curr) {
+//     console.log(curr * amount);
+// }
+// convert(500, usdCurr);
+// convert(500, eurCurr);
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     console.log(curr * amount);
+//     return curr * amount;
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+// const res = convert(500, usdCurr);
+// promotion(res);
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) {
+//             return;
+//         }
+
+//     }
+//     console.log('done');
+// }
+// test();
+
+function sayHello(name) {
+
+    return `Привет, ${name}!`;
+
+
+};
+sayHello('Саша');
+console.log(sayHello);
+
+function returnNeighboringNumbers(num) {
+    // console.log([num - 1, num, num + 1]);
+    return [num - 1, num, num + 1];
+
+
+}
+returnNeighboringNumbers(5);
+console.log(returnNeighboringNumbers(5));
+
+function getMathResult(num, times) {
+    if (typeof(times) !== 'number' || times <= 0) {
+        return num;
+    }
+
+    let str = '';
+
+    for (let i = 1; i <= times; i++) {
+        if (i === times) {
+            str += `${num * i}`;
+
+        } else {
+            str += `${num * i}---`;
+
+        }
+
+    }
+
+    return str;
+}
+
+console.log(getMathResult(5, 50));
+
+const res = getMathResult;
+console.log(res(5, 50));
--- JSB/JS/script.js
+++ JSB/JS/script.js
@@ -1,4 +1,4 @@
-"User strict";
+// "User strict";
 
 // const arr = [1, 2, 3];
 // const arrObj = {
@@ -234,87 +234,122 @@
 // };
 
 
-for (i = 5; i <= 10; i++) {
-    console.log(i);
-};
+// for (i = 5; i <= 10; i++) {
+//     console.log(i);
+// };
 
 
-for (let i = 20; i > 9; i--) {
-    if (i == 12) {
-        break;
-    };
-    console.log(i);
-};
+// for (let i = 20; i > 9; i--) {
+//     if (i == 12) {
+//         break;
+//     };
+//     console.log(i);
+// };
 
-for (let i = 2; i <= 10; i++) {
-    if (i % 2 == 0) {
-        console.log(i);
-    }
+// for (let i = 2; i <= 10; i++) {
+//     if (i % 2 == 0) {
+//         console.log(i);
+//     }
 
-}
+// }
 
 
-for (let i = 2; i <= 16; i++) {
-    if (i % 2 === 0) {
-        continue;
-    } else {
-        console.log(i);
-    }
-}
-let num = 50;
-// // while (num <= 55) {
-// //     console.log(num);
-// //     num++;
-// // }
+// for (let i = 2; i <= 16; i++) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//     }
+// }
+// let num = 50;
+// // // while (num <= 55) {
+// // //     console.log(num);
+// // //     num++;
+// // // }
 
-let i = 2;
-while (i <= 16) {
-    if (i % 2 === 0) {
-        continue;
-    } else {
-        console.log(i);
-        i++;
-    }
-}
-let result = [];
-for (let i = 5; i < 11; i++) {
-    result.push(i++);
-    console.log(i);
-}
+// let i = 2;
+// while (i <= 16) {
+//     if (i % 2 === 0) {
+//         continue;
+//     } else {
+//         console.log(i);
+//         i++;
+//     }
+// }
+// let result = [];
+// for (let i = 5; i < 11; i++) {
+//     result.push(i++);
+//     console.log(i);
+// }
 
-const arrayOfNumbers = [];
+// const arrayOfNumbers = [];
 
-for (let i = 5; i < 11; i++) {
-    arrayOfNumbers[i - 5] = i;
-}
+// for (let i = 5; i < 11; i++) {
+//     arrayOfNumbers[i - 5] = i;
+// }
 
-console.log(arrayOfNumbers);
-return arrayOfNumbers;
+// console.log(arrayOfNumbers);
+// return arrayOfNumbers;
 
-for (let i = 20; i > 9; i--) {
-    if (i === 13) break;
+// for (let i = 20; i > 9; i--) {
+//     if (i === 13) break;
 
-    console.log(i);
-}
+//     console.log(i);
+// }
 
-const arr = [3, 5, 8, 16, 20, 23, 50];
-const result = [];
+// const arr = [3, 5, 8, 16, 20, 23, 50];
+// const result = [];
 
-for (let i = 0; i < arr.length; i++) {
-    result[i] = arr[i];
-}
+// for (let i = 0; i < arr.length; i++) {
+//     result[i] = arr[i];
+// }
 
-console.log(result);
-return result;
+// console.log(result);
+// return result;
 
-const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
 
-for (let i = 0; i < data.length; i++) {
-    if (typeof(data[i]) === 'number') {
-        data[i] = data[i] * 2;
-    } else if (typeof(data[i]) === 'string') {
-        data[i] = `${data[i]} - done`;
-    }
-}
-console.log(data);
-return data;
+// for (let i = 0; i < data.length; i++) {
+//     if (typeof(data[i]) === 'number') {
+//         data[i] = data[i] * 2;
+//     } else if (typeof(data[i]) === 'string') {
+//         data[i] = `${data[i]} - done`;
+//     }
+
+// }
+
+// console.log(data);
+// return data;
+
+
+// const data = [5, 10, 'Shopping', 20, 'Homework'];
+// const result = [];
+
+
+
+// for (let i = 1; i <= data.length; i++) {
+//     result[i - 1] = data[data.length - i]
+// }
+
+// console.log(result);
+
+
+
+// const lines = 5;
+// let result = '';
+
+// for (let i = 0; i <= lines; i++) {
+//     for (let j = 0; j < lines - i; j++) {
+//         result += " ";
+//     }
+//     for (let j = 0; j < 2 * i + 1; j++) {
+//         result += "*";
+//     }
+//     result += "\n";
+// }
+
+// console.log(result)
+
+
+// // Не трогаем
+// return result;
