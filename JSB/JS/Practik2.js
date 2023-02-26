b333b66d30f59b495f894d7275eb1ded83f7e540
Sasha <LLIHUP@gmail.com>
2023-02-26 19:00:38 +0200

    clone

--- JSB/JS/Practik2.js
+++ JSB/JS/Practik2.js
@@ -465,13 +465,171 @@
 // //console.log(rememberMyFilms());
 // console.log(personalMovieDB);
 
-function learnJS(lang, callback) {
-    console.log(`я учу: ${lang}`);
-    callback();
+// function learnJS(lang, callback) {
+//     console.log(`я учу: ${lang}`);
+//     callback();
+// }
+
+// function done() {
+//     console.log('Я прошел этот урок!');
+// }
+
+// learnJS('JS', done);
+
+// const options = {
+//     name: 'test',
+//     width: 1024,
+//     height: 1024,
+//     colors: {
+//         border: 'black',
+//         bg: 'red'
+//     },
+//     makeTest: function() {
+//         console.log('Test'); //
+//     }
+// };
+
+// options.makeTest();
+
+// const { border, bg } = options.colors;
+// console.log(border);
+
+//console.log(Object.keys(options).length);
+
+// console.log(options.name);
+
+// delete options.name;
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {
+//     if (typeof(options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имееи значение ${options[key][i]} `);
+//             counter++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имееи значение ${options[key]} `);
+//         counter++;
+//     }
+
+// }
+
+// console.log(counter);
+
+
+// const arr = [2, 25, 43, 16, 3];
+
+// arr.sort(compareNum);
+// console.log(arr);
+
+// function compareNum(a, b) {
+//     return a - b;
+// }
+// arr[99] = 0;
+// console.log(arr.length);
+// console.log(arr);
+// arr.forEach(function(item, i, arr) {
+// console.log(`${i}: ${item} внутри массива ${arr}`)
+// });
+
+//arr.pop();
+//arr.push(10);
+//console.log(arr);
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// const str = prompt("", "");
+// const product = str.split(", ");
+// product.sort();
+// console.log(product.join('; '));
+
+
+let a = 5,
+    b = a;
+b = b + a;
+console.log(b);
+console.log(a);
+
+const obj = {
+    a: 5,
+    b: 1
+};
+// const copy = obj; //ссылка
+// copy.a = 10;
+// console.log(copy);
+// console.log(obj);
+
+function copy(mainObj) {
+    let objCopy = {};
+    let key;
+    for (key in mainObj) {
+        objCopy[key] = mainObj[key];
+    }
+    return objCopy;
 }
 
-function done() {
-    console.log('Я прошел этот урок!');
+const numbers = {
+    a: 2,
+    b: 5,
+    c: {
+        x: 7,
+        y: 4
+    }
+};
+
+const newNumbers = copy(numbers);
+newNumbers.a = 10;
+console.log(newNumbers);
+console.log(numbers);
+
+
+const add = {
+    d: 17,
+    e: 20
+};
+
+//console.log(Object.assign(numbers, add));
+const clone = Object.assign({}, add);
+
+clone.d = 20;
+// console.log(add);
+// console.log(clone);
+
+const oldArr = ['a', 'b', 'c'];
+const newArr = oldArr.slice();
+newArr[1] = 'asasa';
+console.log(newArr);
+console.log(oldArr);
+
+const video = ['youtube', 'vimeo', 'rutube'],
+    blogs = ['wordpress', 'livejournal', 'blogger'],
+    internet = [...video, ...blogs, 'inst', 'facebook'];
+console.log(internet);
+
+function log(a, b, c) {
+    console.log(a);
+    console.log(b);
+    console.log(c);
+
 }
 
-learnJS('JS', done);
+const num = [2, 5, 7];
+
+
+log(...num);
+
+
+const array = ['a', 'b'];
+const newArray = [...array];
+console.log(newArray);
+
+const q = {
+    one: 1,
+    two: 2
+};
